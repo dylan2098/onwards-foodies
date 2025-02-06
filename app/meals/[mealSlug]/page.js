e0efc0a479518b50f192e312fa5ce27330f7e42f
Tri Nguyen <triminh.nguyen2098@gmail.com>
2025-02-06 22:09:02 +0700

    init project foodies nextjs

---
+++ app/meals/[mealSlug]/page.js
@@ -0,0 +1,38 @@
+import { getMeal } from '@/lib/meals';
+import classes from './page.module.css';
+import Image from 'next/image';
+import { notFound } from 'next/navigation';
+
+export default async function MealDetailPage({params}) {
+  const meal = await getMeal(params.mealSlug);
+
+  if(!meal) {
+    notFound();
+  }
+
+  meal.instructions = meal.instructions.replace(/\n/g, '<br/>');
+
+
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image fill src={meal.image}></Image>
+        </div>
+
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main className={classes.instructions} dangerouslySetInnerHTML={{
+        __html: meal.instructions,
+      }}>
+
+      </main>
+    </>
+  );
+}
---
+++ app/meals/page.js
@@ -0,0 +1,31 @@
+import { getMeals } from '@/lib/meals';
+import MealsGrid from './meals-grid';
+import classes from './page.module.css';
+import { Suspense } from 'react';
+import Link from 'next/link';
+
+async function Meals() {
+  const meals = await getMeals();
+  return <MealsGrid meals={meals} />;
+}
+
+export default function MealsPage() {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious Meal, created <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>Choose your favorite recipe and cook it yourself. It is easy and fun!</p>
+        <p className={classes.cta}>
+          <Link href='/meals/share'>Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <Suspense fallback={<p className={classes.loading}>Fetching meals ...</p>}>
+          <Meals />
+        </Suspense>
+      </main>
+    </>
+  );
+}
